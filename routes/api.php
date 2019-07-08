4e4424d0587d294c2251f283597f39d4fe50bd49
Laurentius Kevin <laurentiuskevin44@gmail.com>
2019-07-08 15:13:42 +0700

    Remove some API Route

--- routes/api.php
+++ routes/api.php
@@ -17,17 +17,8 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('ms-area','MsAreaController@index');
-Route::post('ms-area/add','MsAreaController@add');
-
-Route::get('ms-user','MsUserController@index');
-Route::post('ms-user/add','MsUserController@add');
-Route::post('ms-user/edit','MsUserController@edit');
 Route::post('ms-user/login','MsUserController@loginAndroid');
 
-Route::post('ms-leasing/add','MasterLeasing@add');
-
-Route::post('ms-userarea/add','MsUserAreaController@add');
 Route::post('ms-userarea/check','MsUserAreaController@checkArea');
 
 Route::post('spk/search','AndroidSPK@search');
